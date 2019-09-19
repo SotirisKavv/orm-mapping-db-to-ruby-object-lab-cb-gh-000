1d9b624d404756207a0db8d3cda6ba438143d99f
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-19 19:45:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -69,14 +69,14 @@ class Student
     DB[:conn].execute(sql).collect {|row| self.new_from_db(row)}
   end
 
-  def self.first_X_students_in_grade_10(limit)
+  def self.first_X_students_in_grade_10(X)
     sql = <<-SQL
       SELECT * FROM students
       WHERE grade = "10"
       LIMIT ?
     SQL
 
-    DB[:conn].execute(sql, limit).collect {|row| self.new_from_db(row)}
+    DB[:conn].execute(sql, X).collect {|row| self.new_from_db(row)}
   end
 
   def self.first_student_in_grade_10
